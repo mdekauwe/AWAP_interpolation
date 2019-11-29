83af40b3555ad988889875d49602766b57854bce
mdekauwe <mdekauwe@gmail.com>
2019-11-29 15:34:56 +1100

    Added rainfall script

---
+++ generate_30min_rainfall_forcing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Input forcing for CABLE is 3 hrly, generate 30 min rainfall inputs
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (11.09.2019)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import xarray as xr
+import numpy as np
+import sys
+import glob
+import subprocess
+import pandas as pd
+
+def main(fpath, year):
+
+    fname = "%s/AWAP.Rainf.3hr.%d.nc" % (fpath, year)
+    ds = xr.open_dataset(fname)
+    rain = ds.Rainf
+
+    # Repeat rainfall data and then divide by increased number of timesteps so
+    # to maintain the same rainfall 
+    new_rain = np.repeat(rain, 6, axis=0)
+    new_rain /= 6.0
+
+    # Generate new time sequence
+    dates = pd.date_range(start='1/1/%s 00:00:00' % (str(year)),
+                          periods=len(new_rain),
+                          freq="30min")
+
+    # Create new 30 min rainfall data
+    new_rain['time'] = dates
+    ofname = "awap_30min_rain/AWAP.Rainf.3hr.%d.nc" % (year)
+    new_rain.to_netcdf(ofname)
+
+if __name__ == "__main__":
+
+    # Expecting var to be supplied on cmd line, e.g.
+    # $ python generate_30min_rainfall_forcing.py 1995
+    if len(sys.argv) < 2:
+        raise TypeError("Expecting year name to be supplied on cmd line!")
+
+    year = sys.argv[1]
+    fpath = "/short/w35/mm3972/data/AWAP_to_netcdf"
+
+    main(fpath, year)
