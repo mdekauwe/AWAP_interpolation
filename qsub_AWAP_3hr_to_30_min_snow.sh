3a546ed85b9c9bff75f54d1866a4db72838cf841
mdekauwe <mdekauwe@gmail.com>
2020-03-03 21:21:10 +1100

    Added snow qsub

---
+++ qsub_AWAP_3hr_to_30_min_snow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -m ae
+#PBS -P w35
+#PBS -q normal
+#PBS -M mdekauwe@gmail.com
+#PBS -l mem=128GB
+#PBS -l ncpus=1
+#PBS -l walltime=04:00:00
+#PBS -l wd
+#PBS -j oe
+#PBS -l storage=gdata/w35+gdata/wd9
+
+module load dot
+module add netcdf/4.7.1
+source activate sci
+
+python ./generate_30min_snowfall_forcing.py 1995 2010
+#python ./generate_30min_snowfall_forcing.py 2003 2010
